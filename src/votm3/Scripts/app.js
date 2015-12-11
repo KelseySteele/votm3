33765f01fd3dad0224e37301c620137ac1f04606
Kelsey Steele <kasteele8@gmail.com>
2015-12-11 16:58:13 -0500

    json attempt number one

--- src/votm3/Scripts/app.js
+++ src/votm3/Scripts/app.js
@@ -1,48 +1,50 @@
 ﻿(function () {
     'use strict';
-    var app = angular.module('votm', []);
+    var app = angular.module('votm', ['ngRoute', 'ngSanitize', 'ui.bootstrap']);
 
-    app.controller('PanelController', function () {
-        this.tab = 1;
-        this.setTab = function (setTab) {
-            this.tab = setTab;
-        };
-        this.isSelected = function (checkTab) {
-            return this.tab === checkTab;
-        };
-    })
+    //app.controller('PanelController', function () {
+    //    this.tab = 1;
+    //    this.setTab = function (setTab) {
+    //        this.tab = setTab;
+    //    };
+    //    this.isSelected = function (checkTab) {
+    //        return this.tab === checkTab;
+    //    };
+    //})
 
     //replaced this: angular.module('votm', ['ngRoute', 'ngSanitize']) with app
 
-        app.config(['$routeProvider', function ($routeProvider) {
+    app.config(['$routeProvider', function ($routeProvider) {
 
-            $routeProvider .when('/home', {
-                templateUrl: 'views/home.html'
-            })
-            .when('/overview', {
-                templateUrl: 'views/overview.html',
-                controller: 'PanelController',
-                controllerAs: 'panel'
-            })
-            .when('/instructions', {
-                templateUrl: 'views/instructions.html',
-                controller: 'instructionsCtrl',
-                controllerAs: 'ictrl'
-            })
-             .when('/faq', {
-                 templateUrl: 'views/faq.html',
-                 controller: 'faqCtrl',
-                 controllerAs:'fctrl'
-             })
-            .when('/checklist', {
-                templateUrl: 'views/checklist.html',
-                controller: 'checklistCtrl',
-                controllerAs:'cctrl'
-            })
-            .otherwise({ redirectTo: '/home' });
+        $routeProvider.when('/home', {
+            templateUrl: 'views/home.html'
+        })
+        .when('/overview', {
+            templateUrl: 'views/overview.html',
+            controller: 'overviewCtrl',
+            controllerAs: 'octrl'
+        })
+        .when('/instructions', {
+            templateUrl: 'views/instructions.html',
+            controller: 'instructionsCtrl',
+            controllerAs: 'ictrl'
+        })
+         .when('/faq', {
+             templateUrl: 'views/faq.html',
+             controller: 'faqCtrl',
+             controllerAs: 'fctrl'
+         })
+        .when('/checklist', {
+            templateUrl: 'views/checklist.html',
+            controller: 'checklistCtrl',
+            controllerAs: 'cctrl'
+        })
+        .otherwise({ redirectTo: '/home' });
 
-        }]);
+    }]);
 
-   
+
 
 })();
+
+
