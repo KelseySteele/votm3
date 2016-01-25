817684386d61a63dbe61f52b18a1cc8a1227540b
Kelsey Steele <kasteele8@gmail.com>
2016-01-25 14:25:49 -0500

    First attempt at using grunt-gh-pages

--- src/votm3/gruntfile.js
+++ src/votm3/gruntfile.js
@@ -1,11 +1,13 @@
-﻿/*
+﻿/// <binding BeforeBuild='default' />
+/*
 This file in the main entry point for defining grunt tasks and using grunt plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
 */
 	module.exports = function (grunt) {  
 	    grunt.loadNpmTasks('grunt-contrib-uglify');
 	    grunt.loadNpmTasks('grunt-contrib-watch');
-	
+	    grunt.loadNpmTasks('grunt-gh-pages');
+
 	    grunt.initConfig({
 	        'gh-pages': {
 	            options: {
